04454ca64e211c99552cf6239833d6dc30d19b7f
APKA5CD6RAKDQTNHI5WN <spaintola@deloitte.com>
2020-06-28 18:41:47 +0530

    changed the unit test logic

--- test_calc.py
+++ test_calc.py
@@ -4,7 +4,7 @@ import calc
 class TestCalc(unittest.TestCase):
     def test_add(self):
         result=calc.add(10,5)
-        self.assertEqual(result,15)
+        self.assertEqual(result,11)
 
 
 if __name__ == '__main__':
